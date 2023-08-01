d88ab64fe875e34d1a02ecf1004326047df86fcf
zhangyuhan <605286079@qq.com>
2023-08-01 15:47:53 +0800

    类

---
+++ day-code/src/com/tencent/demo2/lei.java
@@ -0,0 +1,10 @@
+package com.tencent.demo2;
+
+public class lei {
+    public static void main(String[] args) {
+        System.out.println(play(1,3));
+    }
+    public static int play(int num1, int num2){
+        return (num1+num2);
+    }
+}
