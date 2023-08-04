273f245db899a6f90a7c4714c4c6e37a425be4c7
zhangyuhan <605286079@qq.com>
2023-08-04 23:51:18 +0800

    8.4

---
+++ day-code/src/com/tencent/xuelei/Phone.java
@@ -0,0 +1,12 @@
+package com.tencent.xuelei;
+
+public class Phone {
+    String brand;
+    double price;
+    static int number;
+    public void call(){
+        System.out.println("dingling~~~~~");
+        price = 43243;
+        number = 23;
+    }
+}
---
+++ day-code/src/com/tencent/xuelei/YongDianhua.java
@@ -0,0 +1,10 @@
+package com.tencent.xuelei;
+
+public class YongDianhua {
+    public static void main(String[] args) {
+        Phone p = new Phone();
+        p.call();
+        p.brand = "ndj";
+        System.out.println(p.brand);
+    }
+}
