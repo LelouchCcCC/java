0aae4780bb9550aca688e8f9906b231763e7d4ab
zhangyuhan <605286079@qq.com>
2023-09-29 22:34:39 -0400

    spider

---
+++ day-code/src/com/tencent/pachong/Pachong.java
@@ -0,0 +1,16 @@
+package com.tencent.pachong;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class Pachong {
+    public static void main(String[] args) throws MalformedURLException {
+//        URL url = new URL("https://www.baidu.com");
+//        URLConnection conn = url.openConnection();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+    }
+}
